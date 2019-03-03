48476956e8cd2cd3b00122c9c4103b47828acad5
tdriver <driverte@gmail.com>
2019-03-03 07:14:48 -0700

    Adding Airspace classes

---
+++ AGI.AnalyticalServices/Inputs/Airspaces/AirspaceCategories.cs
@@ -0,0 +1,13 @@
+namespace AGI.AnalyticalServices.Inputs.Airspaces
+{
+    public enum AirspaceCategory
+    {
+        Default,
+        Airport,
+        ControlledAirspace,
+        SpecialUseArea,
+        Restricted,
+        Parks,
+        Laanc
+    }
+}
