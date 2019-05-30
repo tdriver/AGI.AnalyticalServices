228d4befbb45b9e60d6f51ccb7d40075b41c813e
Ted Driver <driverte@gmail.com>
2019-05-30 14:49:49 -0600

    Finishing the AccessTest for the ISS

--- AGI.AnalyticalServices.Tests/Access/AccessTests.cs
+++ AGI.AnalyticalServices.Tests/Access/AccessTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Text;
 using AGI.AnalyticalServices.Inputs;
 using AGI.AnalyticalServices.Inputs.Access;
@@ -14,41 +15,32 @@ namespace AGI.AnalyticalServices.Tests.Access
 {
     [TestFixture]
     public class AccessTests
-    {       
+    {
         [Test]
         public void TestSatellitePasses()
         {
             var passRequest = new SatelliteAccessPassData<IVerifiable>();
-            passRequest.SSCs.Add(25544);
-            passRequest.Start = new DateTime(2019,5,27,12,0,0,DateTimeKind.Utc);
+            passRequest.SSCs.Add(25544); // the International Space Station
+            passRequest.Start = new DateTime(2019, 5, 27, 12, 0, 0, DateTimeKind.Utc);
             passRequest.Stop = passRequest.Start.AddDays(7);
             var sd = new SiteData();
-            sd.Location = new ServiceCartographic(40.0,-75.0,0.0);
+            sd.Location = new ServiceCartographic(40.0, -75.0, 0.0);
             passRequest.FromObjectPath = sd;
             passRequest.ToObjectLit = true;
             passRequest.FromObjectDark = true;
+            passRequest.IncludePathCzml = true;
             passRequest.Verify();
 
             // call the service
-            var passResults = AccessServices.GetSatellitePasses<SatellitePassResults<ServiceCartographic>>(passRequest).Result;
-            Assert.That(passResults.Count > 0);
-            // if(passResults.Count > 0){
-            //     // parse results and output
-            //     foreach (var SSC in passResults)
-            //     {
-            //         Console.WriteLine($"There are {SSC.Passes.Count} passes for {SSC.Passes[0].SSC}..");
-            //         int passNumber = 1;
-            //         foreach (var pass in SSC.Passes)
-            //         {
-            //             Console.WriteLine($"------- Pass number {passNumber} ---------");
-            //             Console.WriteLine(pass.ToString());
-            //             passNumber++;
-            //         }
-            //     }
-            // }
-            // else{
-            //     Console.WriteLine("No pass results for those criteria and time span.");
-            // }
+            var passResults = AccessServices.GetSatellitePasses<SatellitePassResults<ServiceCartographicWithTime>>
+                                                                                                (passRequest).Result;
+            Assert.That(passResults.Passes.Count > 0);
+            Assert.That(!string.IsNullOrEmpty(passResults.CzmlForPasses));
+            // Head to cesiumjs.org, click the "Tap to Interact" button. Once the globe appears, Drag the file produced 
+            // in the next line to the globe.  Use the globe controls to move forward and backward in time, and move the 
+            // camera to see the orbit, and highlighted visibility times.
+            File.WriteAllText("ISS.czml", passResults.CzmlForPasses);
+
         }
     }
 }
